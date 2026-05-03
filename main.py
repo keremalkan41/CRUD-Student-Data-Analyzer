7de9b71b5d170a931f67db02159686df84d72b2c
Kerem Alkan <keremalkan007@gmail.com>
2026-05-03 23:33:57 +0300

    feat: add data entry and update panel UI

--- main.py
+++ main.py
@@ -65,11 +65,51 @@ btn5.grid(row=5,column=0,padx=5,pady=5)
 btn6 = tk.Button(lbf1,text="Search",bg="white",fg="black",width=25,highlightbackground="black",highlightthickness=0)
 btn6.grid(row=6,column=0,padx=5,pady=5)
 
-#write here -> (label)
-#lbl1 = tk.Label(lbf1,text="Search:",bg="black",fg="white",padx=5,pady=5)
-#lbl1.grid(row=4,column=1,padx=5,pady=5)
+#labelframe 2
+lbf2 = tk.LabelFrame(root,text="Add Data & Update Data",bg="black",fg="white",padx=10,pady=20)
+lbf2.place(x=350,y=20)
+
+lbl2 = tk.Label(lbf2,text="Name & Surname:",bg="black",fg="white",padx=5,pady=5)
+lbl2.grid(row=0,column=0,padx=5,pady=5,sticky="w")
+
+entry_name = tk.Entry(lbf2,bg="gray",fg="black",width=22,highlightbackground="black",highlightthickness=0)
+entry_name.grid(row=0,column=1,padx=5,pady=5)
+
+lbl3 = tk.Label(lbf2,text="Department:",bg="black",fg="white",padx=5,pady=5)
+lbl3.grid(row=1,column=0,padx=5,pady=5,sticky="w")
+
+entry_department = tk.Entry(lbf2,bg="gray",fg="black",width=22,highlightbackground="black",highlightthickness=0)
+entry_department.grid(row=1,column=1,padx=5,pady=5)
+
+lbl4 = tk.Label(lbf2,text="Number:",bg="black",fg="white",padx=5,pady=5)
+lbl4.grid(row=2,column=0,padx=5,pady=5,sticky="w")
+
+entry_number = tk.Entry(lbf2,bg="gray",fg="black",width=22,highlightbackground="black",highlightthickness=0)
+entry_number.grid(row=2,column=1,padx=5,pady=5)
+
+lbl5 = tk.Label(lbf2,text="GPA:",bg="black",fg="white",padx=5,pady=5)
+lbl5.grid(row=3,column=0,padx=5,pady=5,sticky="w")
+
+entry_gpa = tk.Entry(lbf2,bg="gray",fg="black",width=22,highlightbackground="black",highlightthickness=0)
+entry_gpa.grid(row=3,column=1,padx=5,pady=5)
+
+lbl6 = tk.Label(lbf2,text="Class:",bg="black",fg="white",padx=5,pady=5)
+lbl6.grid(row=4,column=0,padx=5,pady=5,sticky="w")
+
+entry_class = tk.Entry(lbf2,bg="gray",fg="black",width=22,highlightbackground="black",highlightthickness=0)
+entry_class.grid(row=4,column=1,padx=5,pady=5)
+
+lbl7 = tk.Label(lbf2,text="ID (VERY IMPORTANT):",bg="black",fg="white",padx=5,pady=5)
+lbl7.grid(row=5,column=0,padx=5,pady=5,sticky="w")
+
+entry_id = tk.Entry(lbf2,bg="gray",fg="black",width=22,highlightbackground="black",highlightthickness=0)
+entry_id.grid(row=5,column=1,padx=5,pady=5)
+
+btn7 = tk.Button(lbf2,text="ADD",bg="gray",fg="black",highlightbackground="black",highlightthickness=0,width=10)
+btn7.grid(row=6,column=0,padx=5,pady=5,sticky="w")
 
 
-
+btn8 = tk.Button(lbf2,text="UPDATE BY ID",bg="gray",fg="black",highlightbackground="black",highlightthickness=0,width=20)
+btn8.grid(row=6,column=1,padx=5,pady=5,sticky="")
 
 root.mainloop()
