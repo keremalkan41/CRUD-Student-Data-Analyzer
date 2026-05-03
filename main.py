1917851b10c6cf05118f4a6a17adf0da25c93681
Kerem Alkan <keremalkan007@gmail.com>
2026-05-03 20:15:00 +0300

    feat: add mssql connection via env

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import os
 from dotenv import load_dotenv
 import matplotlib.pyplot as plt
 import tkinter as tk
+
+load_dotenv()
+server = os.getenv('DB_SERVER')
+user = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
+database = os.getenv('DB_DATABASE')
+
+try:
+    conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={user};PWD={password}'
+    conn = pyodbc.connect(conn_str)
+    print("successful")
+    conn.close()
+except Exception as e:
+    print(f"CConnection failed: {e}")
