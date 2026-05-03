1ee5bbff501916bc478482c8bef27319aab4f983
Kerem Alkan <keremalkan007@gmail.com>
2026-05-03 23:10:33 +0300

    feat: add search and commands panel UI

--- main.py
+++ main.py
@@ -17,4 +17,59 @@ try:
     print("successful")
     conn.close()
 except Exception as e:
-    print(f"CConnection failed: {e}")
+    print(f"Connection failed: {e}")
+
+#root settings
+root = tk.Tk()
+root.title("Student CRUD System")
+root.geometry("800x800")
+root.resizable(False,False)
+root.config(background="black")
+root.grid_columnconfigure(0, weight=1)
+
+#functions
+#-- Functions will be here --
+def close_program():
+    search_entry.delete(0, "end")
+    root.destroy()
+
+
+#labelframe 1
+lbf1 = tk.LabelFrame(root,text="Search & Commands",bg="black",fg="white",padx=10,pady=20)
+lbf1.place(x=20,y=20)
+
+#buttons(labelframe 1)
+btn1 = tk.Button(lbf1,command=close_program,text="Close Program",bg="white",fg="black",width=25,highlightbackground="black",highlightthickness=0)
+btn1.grid(row=0,column=0,padx=5,pady=5)
+
+btn2 = tk.Button(lbf1,text="Sort By GPA",bg="white",fg="black",width=25,highlightbackground="black",highlightthickness=0)
+btn2.grid(row=1,column=0,padx=5,pady=5)
+
+btn3 = tk.Button(lbf1,text="Clean List",bg="white",fg="black",width=25,highlightbackground="black",highlightthickness=0)
+btn3.grid(row=2,column=0,padx=5,pady=5)
+
+btn4 = tk.Button(lbf1,text="Show 1. Year Students",bg="white",fg="black",width=25,highlightbackground="black",highlightthickness=0)
+btn4.grid(row=3,column=0,padx=5,pady=5)
+
+#text
+lbl1 = tk.Label(lbf1,text="Search:",bg="black",fg="white",padx=5,pady=5)
+lbl1.grid(row=4,column=0,padx=5,pady=5,sticky="w")
+
+#entry for search and also search by ID
+search_entry = tk.Entry(lbf1,bg="gray",fg="black",width=22,highlightbackground="black",highlightthickness=0)
+search_entry.grid(row=4,column=0,padx=5,pady=5,sticky="e")
+
+btn5 = tk.Button(lbf1,text="Search by ID",bg="white",fg="black",width=25,highlightbackground="black",highlightthickness=0)
+btn5.grid(row=5,column=0,padx=5,pady=5)
+
+btn6 = tk.Button(lbf1,text="Search",bg="white",fg="black",width=25,highlightbackground="black",highlightthickness=0)
+btn6.grid(row=6,column=0,padx=5,pady=5)
+
+#write here -> (label)
+#lbl1 = tk.Label(lbf1,text="Search:",bg="black",fg="white",padx=5,pady=5)
+#lbl1.grid(row=4,column=1,padx=5,pady=5)
+
+
+
+
+root.mainloop()
